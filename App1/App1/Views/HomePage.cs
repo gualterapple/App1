ba46578f264a07ac7f88f552ef45cd57fd960c4f
Gualter <gualter_apple@hotmail.com>
2018-09-21 01:58:14 +0100

    hi

---
+++ App1/App1/Views/HomePage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace App1.Views
+{
+    public class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            this.Title = "Select Option";
+            StackLayout stackLayout = new StackLayout();
+            Button button = new Button();
+            button.Text = "Add Company";
+            button.Clicked += Button_Clicked;
+            stackLayout.Children.Add(button);
+
+            button = new Button();
+            button.Text = "Get";
+            button.Clicked += Button_Get_Clicked;
+            stackLayout.Children.Add(button);
+
+            Content = stackLayout;
+
+
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddCompanyPage());
+        }
+
+        private async void Button_Get_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GetAllCompaniesPage());
+        }
+
+    }
+}
+
